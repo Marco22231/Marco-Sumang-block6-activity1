85cec67bdfae9febb11092e2a8d511cb202b0141
Marco Sumang <mape.sumang.up@phinmaed.com>
2023-12-09 21:14:53 +0800

    Initial Commit

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.example.helloworld
 
 import android.os.Bundle
+import android.widget.EditText
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 
@@ -16,7 +17,10 @@ MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val helloTextView = findViewById<TextView>(R.id.hello_text_view)
-        helloTextView.text = "Hello World!"
+        val usernameTextView = findViewById<TextView>(R.id.username_text_view)
+        usernameTextView.text = "Username:"
+
+        val usernameEditText = findViewById<EditText>(R.id.username_edit_text)
+        usernameEditText.hint = "Enter your username..."
     }
 }
